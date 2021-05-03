ca9eeb767fb53d434411c13bca22e6ee914d2bc8
Mark Felchlin <56429361+mdfelchlin@users.noreply.github.com>
2021-05-03 09:15:29 -0700

    Add files via upload

---
+++ AstraAlgoWebsite/wwwroot/PaymentScript.js
@@ -0,0 +1,24 @@
+﻿createCheckoutSession = function (userId) {
+    const stripe = Stripe("pk_test_51IMONzIkAX7cDP9r31eBOkpKbLQ7B99gzod9hRZMLonM5zSQEhoYdUEVYXRvp7rQpztlMyNTM9sR7Cxez5R0XMFa0038JCwaoH");
+    const startPayment = "https://stripepaymentmanagement.azurewebsites.net/api/CreateSubscription?code=weGCE9v2UaKuWykFCMy9bwj6aYxRMgraKQXW/ZrPT9Bz5lhCBeGTeA==&userId=" + userId;
+    return fetch(startPayment, {
+        method: "POST"
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (session) {
+            return stripe.redirectToCheckout({ sessionId: session.id });
+        })
+        .then(function (result) {
+            // If redirectToCheckout fails due to a browser or network
+            // error, you should display the localized error message to your
+            // customer using error.message.
+            if (result.error) {
+                alert(result.error.message);
+            }
+        })
+        .catch(function (error) {
+            console.error("Error:", error);
+        });
+}
---
+++ AstraAlgoWebsite/wwwroot/scripts/zoom-feature.js
@@ -0,0 +1,7 @@
+﻿$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    $(".zoom img").css({
+        width: (100 + scroll / 20) + "%",
+        height: (100 + scroll / 40) + "%"
+    })
+})
